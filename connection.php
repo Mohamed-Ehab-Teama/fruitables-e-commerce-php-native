7d06fd8e881bede365359a7a42b9294d54fdd545
Mohamed Ehab Mohamed Teama <mohamed111.me111@gmail.com>
2024-11-26 23:06:46 +0200

    Display categories and all products from database

--- connection.php
+++ connection.php
@@ -13,3 +13,5 @@ $connection = mysqli_connect( $host, $DBUserName, $DBUserPass, $DBName );
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+// echo "<img src='data:image/jpeg;base64," . base64_encode($row['image']) . "' alt='{$row['product']}' style='width:150px;'>";
--- functions.php
+++ functions.php
@@ -6,3 +6,24 @@ function get_all_records ( $connection,  $table ) {
     return mysqli_query($connection, $sql);
 }
 
+
+
+function get_all_records_from_column ( $connection, $table1 ,  $column,  $value){
+    $sql = " SELECT * FROM $table1 WHERE $column = '$value' ";
+    return mysqli_query ( $connection, $sql );
+}
+
+
+
+
+function get_all_records_from_two_tables( $connection, $table1 , $table2 , $column1, $column2  ){
+    $sql = " SELECT * FROM $table1 INNER JOIN $table2 ON $column1 = $column2 ";
+    return mysqli_query ( $connection, $sql );
+}
+
+
+function get_all_records_from_two_tables_on_check ( $connection, $table1 , $table2 , $column1, $column2 , $check , $value ){
+    $sql = " SELECT * FROM $table1 INNER JOIN $table2 ON $column1 = $column2 WHERE $check = '$value' ";
+    return mysqli_query ( $connection, $sql );
+}
+
